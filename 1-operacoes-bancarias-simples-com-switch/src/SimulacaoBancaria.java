2db6c786fcc48f997aafce5b18deeae55817fc02
hugopontojava <hugots23@gmail.com>
2024-04-30 20:43:27 -0300

    build: adicionada uma forma mais fácil de encerrar o loop

--- 1-operacoes-bancarias-simples-com-switch/src/SimulacaoBancaria.java
+++ 1-operacoes-bancarias-simples-com-switch/src/SimulacaoBancaria.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
 
 public class SimulacaoBancaria {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double saldo = 0;
-        double valor;
-        boolean loop = true;
         System.out.println("Esse é um simulador bancário");
             // TODO: tratamento de execções e facilitar a compreensão do código
-        while (loop == true) {
+        while (true) {
             System.out.println("------------------ \n" +
                     "Escolha umas das opções: \n" +
                     "1 - Depositar \n" +
@@ -16,7 +14,8 @@ public class SimulacaoBancaria {
                     "3 - Saldo \n" +
                     "0 - Encerrar \n" +
                     "------------------");
-            int opcao = Integer.parseInt(String.valueOf(sc.nextInt()));
+            double valor;
+            int opcao = sc.nextInt();
             switch (opcao) {
                 case 1:
                     System.out.println("Insira o valor a ser depositado...");
@@ -32,12 +31,8 @@ public class SimulacaoBancaria {
                     System.out.println("Você tem R$" + saldo + " de saldo.");
                     break;
                 case 0:
-                    // TODO: achar uma maneira mais "simples" de finalizar o loop
                     System.out.println("Finalizando simulação...");
-                    Thread.sleep(1000);
-                    loop = false;
-                    System.out.println("Simulação encerrada com sucesso!");
-                    break;
+                    System.exit(0);
                 default:
                     System.out.println("Opção inválida. Tente novamente.");
             }
