1f12de6d0b2ba86788916bd648bf6dce460b7714
hugopontojava <hugots23@gmail.com>
2024-05-08 00:00:28 -0300

    build: operações separadas em funções, exceções criadas e try-catch adicionado

--- 1-operacoes-bancarias-simples-com-switch/src/SimulacaoBancaria.java
+++ 1-operacoes-bancarias-simples-com-switch/src/SimulacaoBancaria.java
@@ -4,8 +4,7 @@ public class SimulacaoBancaria {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double saldo = 0;
-        System.out.println("Esse é um simulador bancário");
-            // TODO: tratamento de execções e facilitar a compreensão do código
+
         while (true) {
             System.out.println("------------------ \n" +
                     "Escolha umas das opções: \n" +
@@ -14,28 +13,67 @@ public class SimulacaoBancaria {
                     "3 - Saldo \n" +
                     "0 - Encerrar \n" +
                     "------------------");
-            double valor;
             int opcao = sc.nextInt();
             switch (opcao) {
                 case 1:
-                    System.out.println("Insira o valor a ser depositado...");
-                    valor = sc.nextDouble();
-                    saldo += valor;
+                    System.out.println("INSIRA O VALOR A SER DEPOSITADO...");
+                    try {
+                        saldo = deposita(saldo);
+                    } catch (ValorNaoSuportadoException ex) {
+                        System.out.println(ex.getMessage());
+                    }
                     break;
                 case 2:
-                    System.out.println("Insira o valor a ser sacado...");
-                    valor = sc.nextDouble();
-                    saldo -= valor;
+                    System.out.println("INSIRA O VALOR A SER SACADO...");
+                    try {
+                        saldo = saca(saldo);
+                    } catch (SaldoInsuficienteException ex) {
+                        System.out.println(ex.getMessage());
+                    }
                     break;
                 case 3:
-                    System.out.println("Você tem R$" + saldo + " de saldo.");
+                    System.out.println("Saldo atual: R$" + saldo);
                     break;
                 case 0:
-                    System.out.println("Finalizando simulação...");
+                    System.out.println("Encerrando simulação...");
+                    // Usando System.exit por enquanto porque não tenho um tratamento para o erro de sc.close()
                     System.exit(0);
+                    break;
                 default:
                     System.out.println("Opção inválida. Tente novamente.");
             }
+        }
+    }
+
+    // Operações
+    public static double deposita(double saldo) throws ValorNaoSuportadoException{
+        Scanner sc = new Scanner(System.in);
+        double valor = sc.nextDouble();
+        if (valor <= 0) {
+            throw new ValorNaoSuportadoException("Valor não suportado");
+        } else {
+            return saldo + valor;
+        }
+    }
+    public static double saca(double saldo) throws SaldoInsuficienteException {
+        Scanner sc = new Scanner(System.in);
+        double valor = sc.nextDouble();
+        if (saldo < valor) {
+            throw new SaldoInsuficienteException("Saldo insuficiente");
+        } else {
+            return saldo - valor;
+        }
+    }
+
+    // Exceptions
+    public static class SaldoInsuficienteException extends Exception {
+        public SaldoInsuficienteException(String msg) {
+            super(msg);
+        }
+    }
+    public static class ValorNaoSuportadoException extends Exception {
+        public ValorNaoSuportadoException(String msg) {
+            super(msg);
         }
     }
 }
