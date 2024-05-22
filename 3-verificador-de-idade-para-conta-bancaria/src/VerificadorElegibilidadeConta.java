f02542b6a2dd4472cd12176213b6269fd8b508b5
hugopontojava <hugots23@gmail.com>
2024-05-22 05:53:30 -0300

    build: resolução feita com try-finally e if-else

--- 3-verificador-de-idade-para-conta-bancaria/src/VerificadorElegibilidadeConta.java
+++ 3-verificador-de-idade-para-conta-bancaria/src/VerificadorElegibilidadeConta.java
@@ -3,13 +3,15 @@ import java.util.Scanner;
 public class VerificadorElegibilidadeConta {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        int idade = scanner.nextInt();
-
-        // TODO: Verifique se a idade é maior ou igual a 18 e imprima uma mensagem informando que o usuário é elegível para criar uma conta bancária:
-
-
-        // Fechamos o objeto Scanner para liberar os recursos:
-        scanner.close();
+        try {
+            int idade = scanner.nextInt();
+            if (idade < 18) {
+                System.out.println("Você não está elegível para criar uma conta bancária.");
+            } else {
+                System.out.println("Você está elegível para criar uma conta bancária.");
+            }
+        } finally {
+            scanner.close();
+        }
     }
 }
